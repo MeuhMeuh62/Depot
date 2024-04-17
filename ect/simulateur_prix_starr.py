86517e845429a4a5cb2311a8373241097e7b09ff
MeuhMeuh62 <tom.coutte@proton.me>
2024-04-17 10:52:56 +0200

    🏆 Ajout du gain mythique

--- ect/simulateur_prix_starr.py
+++ ect/simulateur_prix_starr.py
@@ -2,36 +2,36 @@ import random
 
 # données de : https://support.supercell.com/brawl-stars/en/articles/starr-drops-3.html
 
-all_brawlers=["Shelly", "Nita", "Colt", "Bull", "Brock", "El Primo", "Barley", "Poco", "Rosa", "Jessie", "Dynamike", "Tick", "8-Bit", "Rico", "Darryl", "Penny", "Carl", "Jacky", "Gus", "Bo", "Emz", "Stu", "Piper", "Pam", "Frank", "Bibi", "Bea", "Nani", "Edgar", "Griff", "Grom", "Bonnie", "Gale", "Colette", "Belle", "Ash", "Lola", "Sam", "Mandy", "Maisie", "Hank", "Pearl", "Larry & Lawrie", "Angelo", "Mortis", "Tara", "Gene", "Max", "Mr. P", "Sprout", "Byron", "Squeak", "Lou", "Ruffs", "Buzz", "Fang", "Eve", "Janet", "Otis", "Buster", "Gray", "R-T", "Willow", "Doug", "Chuck", "Charlie", "Mico", "Spike", "Crow", "Leon", "Sandy", "Amber", "Meg", "Surge", "Chester", "Cordelius", "Kit"]
 spray_en_plus=["Angel", "Arrow Heart", "Banana Peel", "Breaking Wall", "Champion", "Crying Mask", "Don’t Be Salty", "Denied", "Fire Punch", "Flaming", "Footprint", "Gem King", "Lightning Cloud", "Starr Logo", "Sweat Drop", "Thumbs Up", "Warning", "Tengu Mike", "Kitsune Lola", "Oni Otis", "Hanbok Mandy", "Cheerleader Rosa", "Nerd Squeak", "Bell Nani", "Sunken Chest Griff", "Kraken Surge", "Haunted House 8-Bit", "Stone Troll Lou", "Dark Fairy Janet", "Wood Spirit Chester", "Daruma Mr. P", "Leopard Max", "Sultan Carl", "Tempest Tara", "Scorpion Willow", "Raider Cordelius"]
+gadgets_list=[]
 
 brawlers_rare=["Nita", "Colt", "Bull", "Brock", "El Primo", "Barley", "Poco", "Rosa"]
 brawlers_super_rare=["Jessie", "Dynamike", "Tick", "8-Bit", "Rico", "Darryl", "Penny", "Carl", "Jacky", "Gus"]
 brawlers_epic=["Bo", "Emz", "Stu", "Piper", "Pam", "Frank", "Bibi", "Bea", "Nani", "Edgar", "Griff", "Grom", "Bonnie", "Gale", "Colette", "Belle", "Ash", "Lola", "Sam", "Mandy", "Maisie", "Hank", "Pearl", "Larry & Lawrie", "Angelo"]
 brawlers_mythique=["Mortis", "Tara", "Gene", "Max", "Mr. P", "Sprout", "Byron", "Squeak", "Lou", "Ruffs", "Buzz", "Fang", "Eve", "Janet", "Otis", "Buster", "Gray", "R-T", "Willow", "Doug", "Chuck", "Charlie", "Mico"]
 brawlers_légendaire=["Spike", "Crow", "Leon", "Sandy", "Amber", "Meg", "Surge", "Chester", "Cordelius", "Kit"]
+all_brawlers=brawlers_rare+brawlers_super_rare+brawlers_epic+brawlers_mythique+brawlers_légendaire
 
 skins_rare=["Panda Nita", "Outlaw Colt", "Rockstar Colt", "Golden Barley", "8-Bit Classic", "Constructor Jacky", "Boy Scout Gus", "Goblin Carl", "Warrior Bo", "Mean Girl Emz", "Superstar Stu", "Pink Piper", "Bubblegum Bibi", "Ladybug Bea", "Retro Nani", "Blackbird Edgar", "Sam The Teddy", "MC Mandy", "Tech Maisie", "Shark Tank Hank", "Unicorn Bonnie", "Footbonnie", "Vintage Pearl", "Agent P", "Amazing Charlie", "Tropical Sprout", "Detective Gray", "Runaway Gray", "Kaiju Buzz", "Stara", "GT Max", "Patrol R-T", "Axl Willow", "Draco Doug", "Inferno Chuck", "Mask Spike", "Dark Chester", "Sleepy Sandy", "G-Force Janet", "Leon the Stray", "Poison Cap Cordelius"]
 
 def pin(a,b):
     if a==0:
-        a="heureuse"
+        a="heureux(se)"
     if a==1:
-        a="fâchée"
+        a="fâché(e)"
     if a==2:
         a="triste"
     if a==3:
-        a="GG"
+        a="'GG'"
     if a==4:
-        a="applaudissements"
+        a="qui applaudit"
     if a==5:
-        a="choquée"
+        a="choqué(e)"
     if a==6:
-        a="spéciale"
+        a="spécial(e)"
     if a==7:
-        a="remerciement"
-    print(f"Tu as gagné l'émote {a} de {b}")
-
+        a="qui remercie"
+    print(f"Tu as gagné l'émote de {b} {a}")
 
 def brawler_get(a):
     print(f"Tellemet t'es beau t'as obtenu {a}")
@@ -39,6 +39,11 @@ def brawler_get(a):
 def spray(a):
     print(f"Tu as obtenu le spray qui s'appelle : {a}")
 
+
+
+
+
+# Obtention du gain rare par l'aléatoire (entre 0 et 1)
 def gain_rare(i):
     global coins,power_points,crédits,blings,double_xp
     if 0<=i<0.4190:
@@ -62,6 +67,9 @@ def gain_rare(i):
         double_xp+=200
         print(f"Tu as un total actuel de {double_xp} doubleurs d'xp")
 
+
+
+# Obtention du gain super_rare par l'aléatoire (entre 0 et 1)
 def gain_super_rare(i):
     global coins,power_points,crédits,blings,double_xp
     if 0<=i<0.4238:
@@ -89,6 +97,9 @@ def gain_super_rare(i):
         double_xp+=200
         print(f"Tu as un total actuel de {double_xp} doubleurs d'xp")
 
+
+
+# Obtention du gain epic par l'aléatoire (entre 0 et 1)
 def gain_epic(i):
     global coins,power_points,crédits,blings,double_xp
     if 0<=i<0.2105:
@@ -114,6 +125,23 @@ def gain_epic(i):
         double_xp+=200
         print(f"Tu as un total actuel de {double_xp} doubleurs d'xp")
 
+
+
+# Obtention du gain mythique par l'aléatoire (entre 0 et 1)
+def gain_mythique(i):
+    global coins,power_points,crédits,blings,double_xp
+    if 0<=i<0.0949:
+        print(f"Tu as obtenu : 500 pièces")
+        coins+=500
+        print(f"Tu as un total actuel de {coins} pièces")
+    elif 0.0949<=i<0.2531:
+        gadget(random.choice(gadgets_list))
+
+
+
+
+
+# Ouverture du prix Starr et détermination aléatiore de sa raretée 
 def ouverture(i):
     if 0<=i<0.5:
         print("Prix Starr : Rare")
@@ -126,8 +154,10 @@ def ouverture(i):
         gain_epic(round(random.random(),4))
     elif 0.93<=i<0.98:
         print("Prix Starr : Mythique")
+        print("En travaux : Pour l'instant pas de récompense")
     elif 0.98<=i<=1:
         print("Prix Starr : Légendaire")
+        print("En travaux : Pour l'instant pas de récompense")
 
 fin=False
 coins=0
@@ -137,15 +167,18 @@ blings=0
 double_xp=0
 
 while fin==False:
-    input("Clique sur ENTREE pour ouvrir le prochain prix star !")
-    ouverture(round(random.random(),2))
+    p=input("Clique sur ENTREE pour ouvrir le prochain prix star ! (e + ENTREE pour quitter)")
+    if p == "e":
+        fin=True
+    else:
+        ouverture(round(random.random(),2))
 
-
-
-# print(pièce)
-# print(power_points)
-# print(crédits)
-# print(blings)
-# print(double_xp)
-
-exit()
+print("")
+print("---------- FIN DE LA SIMULATION ----------")
+print("")
+print("Tu as obtenu au total :")
+print(f"{coins} pièces")
+print(f"{power_points} points de pouvoir")
+print(f"{crédits} crédits")
+print(f"{blings} blings")
+print(f"{double_xp} doubleurs d'xp")
